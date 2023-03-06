efba778b8c3678ce1acdfa523f617f87f167efa8
RomanSavitskiy <roma.savitskiy@inbox.ru>
2023-03-06 20:45:19 +0300

    Added test for controller

---
+++ src/test/java/com/example/cicdexample/controller/TestControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.cicdexample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(TestController.class)
+class TestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test1() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello world"));
+    }
+}
